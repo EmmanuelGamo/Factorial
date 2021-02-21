5d8b1c94594ac4d5b4e699e2ec7bd308a89c38da
EmmanuelGamo <58947901+EmmanuelGamo@users.noreply.github.com>
2021-02-21 15:02:28 +0800

    Factorial Using Recursion

---
+++ Factorial Using Recursion/Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            long FactN = GetFactorial(number);
+            Console.WriteLine("{0} factorial is equal to {1}", number, FactN);
+            Console.ReadKey();
+        }
+        private static long GetFactorial(int number) 
+        {
+            if (number == 0)
+            {
+                return 1;
+            }           
+            int diff = number - 1;
+            return number * GetFactorial(diff); // using recursion       
+        }
+    }
+}
